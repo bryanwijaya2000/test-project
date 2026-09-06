e47cb93e5b2b450fafa906777f436f6679bb117f
bryanwijaya2000 <80102606+bryanwijaya2000@users.noreply.github.com>
2026-09-06 15:10:32 +0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+from utils import add, subtract
+
+def foo(a, b):
+  if add(a, b) + subtract(a, b) == 0:
+    return 0
+  if add(a, b) > 0:
+    return 1
+  if subtract(a, b) < 0:
+    return -1
+  return 1000
+
+print(foo(10, 9))
