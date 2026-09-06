3820db1ccb9ff7fd1777c05d81b82ae8c81bb751
Bryan <bryanandrewwijaya2000@gmail.com>
2026-09-06 23:47:46 +0700

    Update file feature1.py add input checking for 0 and 1

--- feature1.py
+++ feature1.py
@@ -1,1 +1,13 @@
 print("This is Feature 1.")
+
+a = int(input())
+if a == 0:
+  print("a is 0")
+else:
+  print("a is not 0")
+
+b = int(input())
+if b == 1:
+  print("b is 1")
+else:
+  print("b is not 1")
